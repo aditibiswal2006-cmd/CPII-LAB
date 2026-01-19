5558c3f321b481e59e7b3e1ff558e6258c2c5bcd
aditibiswal2006-cmd <aditibiswal2006@gmail.com>
2026-01-19 17:11:18 +0530

    Add files via upload

---
+++ 25mimd97/string.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	char name[25];
+	char sicno[10];
+	printf("Enter your name");
+	gets("Name");
+	printf("Enter your sicno");
+	scanf("%s,sicno");
+	printf("Name=%s \n sicno = %s",name,sicno);
+}>
