696d8997bc9e291e256e985a6e1a1e4bf0ec9003
aditibiswal2006-cmd <aditibiswal2006@gmail.com>
2026-01-22 00:01:43 +0530

    Update string2.c

--- 25mimd97/string2.c
+++ 25mimd97/string2.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main(){
-	char test[20] = "Adiyi biswal" ;
+	char test[20] = "Aditi biswal" ;
 	int i = 0;
 	while(test[i]!='\0'){
 		i++ ;
 	}
 	printf("the length of the string is %d\n",i);
 }            
+
